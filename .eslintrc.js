795ef5f2a33243c904afa66a93a029262f423a3b
Miguel Rios <miguel@adianta.com.br>
2020-07-26 16:53:28 -0300

    eslint setup setup

---
+++ .eslintrc.js
@@ -0,0 +1,1 @@
+module.exports = require('@monorepo-flask-react/eslint');
--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
-const App: React.FC = () =>
-  (
-    <h1>this is client</h1>
-  );
-
+const App: React.FC = () => <h1>this is client</h1>;
 
 export default App;
